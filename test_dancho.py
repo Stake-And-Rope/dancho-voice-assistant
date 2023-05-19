7a51e8e344c6efc3a92bc73fb20980768d2713c6
karastoyanov on Windows 11 DIW <karastoqnov.alexandar@gmail.com>
2023-05-19 09:56:50 +0300

    Branch restored

---
+++ test_dancho.py
@@ -0,0 +1,21 @@
+import pyttsx3
+import pyaudio
+import speech_recognition as sr
+
+MASTER = input("Enter your name: ")
+print("Initializing Dancho Voice Assistant")
+
+engine = pyttsx3.init()
+"""READ THE DOCS https://pyttsx3.readthedocs.io/en/latest/engine.html"""
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+speak(f"Hello, {MASTER}")
+
