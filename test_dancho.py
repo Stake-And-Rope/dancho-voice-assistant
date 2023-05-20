a27d5a293141dfebdf8f5456e90b3ec9fea1886d
Daniel Yordanov <theface11311@gmail.com>
2023-05-20 16:21:21 +0300

    Dancho says the current time in 24 hours format.

--- test_dancho.py
+++ test_dancho.py
@@ -1,6 +1,7 @@
 import pyttsx3
 import pyaudio
 import speech_recognition as sr
+from datetime import datetime
 
 MASTER = input("Enter your name: ")
 print("Initializing Dancho Voice Assistant")
@@ -23,13 +24,14 @@ def greeting():
 
 def say_time():
     """Type your code here"""
-    pass
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return current_time
 
 
 def say_date():
     """"Type your code here"""
     pass
 
-# speak(f"Hello, {MASTER}")
-# say_time()
+speak(f"Hello, {MASTER}")
+speak(f"The current time is {say_time()}")
 # say_date()
