86255ad29f231e62ae300c2a7b0fc080f56c9669
karastoyanov on Windows 11 DIW <karastoqnov.alexandar@gmail.com>
2023-05-20 20:18:39 +0300

    Project Update

--- test_dancho.py
+++ test_dancho.py
@@ -2,6 +2,7 @@ import pyttsx3
 import pyaudio
 import speech_recognition as sr
 import time
+import datetime
 
 MASTER = input("Enter your name: ")
 print("Initializing Dancho Voice Assistant")
@@ -29,10 +30,18 @@ def say_time():
 
 
 def say_date():
-    """"Type your code here"""
-    pass
+    cur_date = datetime.date.today().strftime("%d:%m:%Y")
+
+    spec_dates = {"17:03", }
+    spec_dates_prompts = {"17:03": f"It's my birthday."}
+
+    if not spec_dates.issuperset(cur_date[:-5]):
+        speak(f"Today's {cur_date}. It's just a regular day")
+    else:
+        speak(f"Today's {cur_date}. {spec_dates_prompts[cur_date[:-5]]}")
 
 
 speak(f"Hello, {MASTER}")
 speak(f"It is {say_time()}")
-# say_date()
+speak(say_date())
+
