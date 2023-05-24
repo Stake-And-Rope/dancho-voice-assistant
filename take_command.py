e9996873f8723bc8d919eff2779a888f9fd1ee2a
karastoyanov on Windows 11 DIW <karastoqnov.alexandar@gmail.com>
2023-05-24 14:38:23 +0300

    Code Refractor

--- take_command.py
+++ take_command.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
-sys.path.append(r'date_time/')
-sys.path.append(r'wikipedia_search/')
 import pyttsx3
 import speech_recognition as sr
-import wikipedia
+
+"""IMPORT THE SEPARATE EXTERNAL MODULES"""
+sys.path.append(r'date_time/')
+sys.path.append(r'wikipedia_search/')
 import date_time.current_date_time as current_date_time
 import wikipedia_search.article_search as wiki_search
 
@@ -40,15 +41,23 @@ def take_command():
         print("Processing...")
         command = r.recognize_google(audio, language='en-US')
         print(f"User said: {command}\n")
+        
+        """DANCHO TELLS THE CURRENT DATE"""
         if "tell me the date" in command:
             speak(current_date_time.say_date())
+            
+        """DANCHO TELLS THE CURRENT TIME"""
         if "tell me the time" in command:
             speak(f"Right now is: {current_date_time.say_time()}")
+            
+        """DANCHO SEARCHES FOR ARTICLE IN WIKIPEDIA. IT WILL RETURN ONLY THE FIRST THREE SENTENCES"""
         if "Wikipedia" in command:
             command = command.replace("Wikipedia", "")
             result = wiki_search.article_search(str(command))
             speak(f"According to Wikipedia: {result}")
             speak("Do you want to open the full article in your web browser?")
+            
+            
     except Exception as e:
         print(e)
         speak("Say that again please...")
