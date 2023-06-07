83072ce37596885d211e8935972297c2ec7c6aaa
karastoyanov on Windows 11 DIW <karastoqnov.alexandar@gmail.com>
2023-06-07 19:45:49 +0300

    youtube folder added

--- spotify/spotify_auth.py
+++ spotify/spotify_auth.py
@@ -5,4 +5,11 @@
 # https://www.lfd.uci.edu/~gohlke/pythonlibs/#pycurl
 
 import pycurl
+import requests
 
+
+client_id = '1bc6f323006f4e808cc3f699309bdaec'
+client_secret = 'c46f2c7b64bc43ebab1da3ee32e1c62b'
+
+r = requests.get('https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb')
+print(r)
