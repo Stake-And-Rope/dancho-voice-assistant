7e6530041c60b43af493d20ae7d79e3ebb5c2aaf
karastoyanov on ThinkPad X1 <karastoqnov.alexandar@gmail.com>
2023-06-08 20:47:30 +0300

    Youtube functionality updated:  - file renamed for better visibility  - function accepts one argument -> search, the actual string which will be passed to youtube as search word  - .env file implemented with python-decouple library, storing the google token locally`:wq

---
+++ youtube/youtube_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from googleapiclient.discovery import build
+from decouple import config
+import json
+
+api_key = config('GOOGLE_TOKEN')
+
+def search_results(search):
+    youtube = build("youtube", "v3", developerKey=api_key)
+    request = youtube.search().list(q=search, part='snippet', type='playlist')
+    results = request.execute()
+    # print(results)
+    json_results = json.dumps(results)
+    print(json_results)
+    return results
+
+
+
+search_results("how to python to json")
+
+#if __name__ == "__main__":
+#    search_results()
