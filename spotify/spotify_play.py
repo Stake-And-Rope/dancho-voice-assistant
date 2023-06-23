6d8466285b6d38f9e2df808c672fe60602b2b44a
karastoyanov on vivobook <karastoqnov.alexandar@gmail.com>
2023-06-23 22:50:09 +0300

    Spotify play track implemented

--- spotify/spotify_play.py
+++ spotify/spotify_play.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import json
 import spotipy
+import re
 import webbrowser
 from decouple import config
+from spotipy.oauth2 import SpotifyOAuth
 
 
 username = config('SPOTIFY_USER')
@@ -15,4 +17,27 @@ token = token_dict['access_token']
 spotifyObject = spotipy.Spotify(auth=token)
 user_name = spotifyObject.current_user()
 
-print(user_name)
+
+
+scope = "user-read-playback-state,user-modify-playback-state"
+#sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientID,
+                                               client_secret=clientSecret,
+                                               redirect_uri=redirect_uri,
+                                               scope="user-read-playback-state,user-modify-playback-state"))
+
+res = sp.devices()
+#print(json.dumps(sp.current_playback()))
+
+
+def play_track(search):
+    result = sp.search(search, limit=1, type='track')
+    pattern = r'https:\/\/open.spotify.com\/track\/\d\w{1,}'
+    result = re.findall(pattern, str(result))
+    #print(result)
+    sp.start_playback(uris = result)
+
+
+
+play_track('Neon Nox - Checkpoint')
+
